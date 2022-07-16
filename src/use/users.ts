3d6bd3c9863e45947e3c7f804081aaad3fd19408
aplatov76 <aplatov76@gmail.com>
2022-07-16 14:11:44 +0300

    suspend component

--- src/use/users.ts
+++ src/use/users.ts
@@ -1,9 +1,18 @@
 import {ref, Ref} from 'vue';
 import { useFetch } from "./fetch";
 
-export async function useUsers(): Promise<any>{
+interface User {
+    id: number,
+    name: string,
+    email: string
+
+}
+
+type UsableUsers = Promise<{users: Ref<User[] | undefined>}>
+
+export async function useUsers(): UsableUsers{
     const laoding: Ref<boolean> = ref(false)
-    const {response: users, request} = useFetch('https://jsonplaceholder.typicode.com/users');
+    const {response: users, request} = useFetch<User[]>('https://jsonplaceholder.typicode.com/users');
     console.log('request')
     if(!laoding.value){
         await request()
